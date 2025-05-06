53181e055020c2262fe2c08ff731b82efc3ab3c4
aksi <higko@yandex.ru>
2025-05-06 14:26:59 +0300

    change server api

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,3 @@
 // export const SERVER_URL = 'http://7.6.70.200:8000/api'
-export const SERVER_URL = 'https://api.itmfc.ru/api'
+// export const SERVER_URL = 'https://api.itmfc.ru/api'
+export const SERVER_URL = 'https://wvfox-itmfc-server-v2-edce.twc1.net/api'
